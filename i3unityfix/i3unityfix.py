3b0adc328e197b9ca83d0bec459db112809f6b59
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-08-27 14:49:47 +0200

    Listen to workspace instead of focus event to prevent spam bug

--- i3unityfix/i3unityfix.py
+++ i3unityfix/i3unityfix.py
@@ -4,22 +4,32 @@ from i3ipc import Connection, Event
 from pynput.keyboard import Key, Controller
 import subprocess
 
+def window_is_unity(window):
+    if (window is None):
+        return False
+    return window.window_class == "Unity"
+
 class I3UnityFix(object):
 
     def __init__(self):
         self.keyboard = Controller()
-    
-    def on_focus(self, i3, event):
-        if (event.container.window_class == "Unity"):
-            event.container.command("fullscreen enable")
-            event.container.command("fullscreen disable")
-            self.keyboard.press(Key.space)
-            self.keyboard.release(Key.space)
+        
+    def on_workspace_focus(self, i3, event):
+        workspace = event.current
+
+        windows = list(workspace.leaves())
+        windows = filter(window_is_unity, windows)
+
+        for window in windows:
+            window.command("fullscreen enable")
+            window.command("fullscreen disable")
+            self.keyboard.press(Key.esc)
+            self.keyboard.release(Key.esc)
             
 def main():
     i3 = Connection()
     handler = I3UnityFix()
-    i3.on(Event.WINDOW_FOCUS, handler.on_focus)
+    i3.on(Event.WORKSPACE_FOCUS, handler.on_workspace_focus)
     i3.main()
     
 if __name__ == "__main__":
